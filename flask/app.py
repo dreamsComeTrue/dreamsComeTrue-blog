e63861d96dfd544fe6534da0b2b25e40f5167c8f
Dominik 'squ@ll' Jasiński <dominikjasinski@o2.pl>
2019-05-26 17:23:50 +0200

    Added getting started template

--- flask/app.py
+++ flask/app.py
@@ -3,6 +3,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/<string:message>', methods=['GET', 'POST'])
+def hello(message):
+    return f"""<html>
+        <head>
+            <link rel="stylesheet" type="text/css" href="static/main.css">
+        </head>
+        <body>
+            <b>Hello {message}</b>
+        </body>
+    </html>"""
